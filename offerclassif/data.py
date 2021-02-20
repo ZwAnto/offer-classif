b27d5c51607ae94dc67536e63e7013681f92f141
zwanto <antoine.hamon@protonmail.com>
2021-02-20 21:45:39 +0100

    adding pandas formatting func

--- offerclassif/data.py
+++ offerclassif/data.py
@@ -1,4 +1,5 @@
 import json 
+import pandas as pd
 
 def load_json(file):
     """Json loading function
@@ -11,6 +12,22 @@ def load_json(file):
     """
     with open(file) as f:
         data = json.load(f)
-    print(f'{len(data)} loaded from {file}.')
+    print(f'{len(data)} items loaded from {file}.')
     
     return data
+
+
+def get_df(offers, thesaurus=None):
+
+    df = pd.DataFrame([{
+        'id': i.get('employments',[{}])[0].get('employment_id', None),
+        'description': i['description'],
+        'title': i['title'],
+        'text': i['title'] + '. ' + i['description']
+    } for i in offers])
+        
+    if thesaurus:
+        df = pd.merge(pd.DataFrame(thesaurus), df, how='right')
+
+    return df
+
